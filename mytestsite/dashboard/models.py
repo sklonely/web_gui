fc4179c1ca4323468686b5dd558f27179e172556
sklonely <asd195375@gmail.com>
2021-11-29 15:51:30 +0800

    無用更新

--- mytestsite/dashboard/models.py
+++ mytestsite/dashboard/models.py
@@ -1,12 +1,20 @@
-from django.db import models
-
 # Create your models here.
 from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
+    
+    machine_name = models.CharField(max_length=100, primary_key=True)
+    work_no = models.CharField(max_length=100,null=True)
+    principal = models.CharField(max_length=100,null=True)
+
+    production_capacity = models.DecimalField(max_digits=10,decimal_places=0,null=True)
+    need_production_capacity = models.DecimalField(max_digits=10,decimal_places=0,null=True)
+    check_nums = models.DecimalField(max_digits=10,decimal_places=0,null=True)
+    speed = models.DecimalField(max_digits=10,decimal_places=2,null=True)
+    good_production = models.DecimalField(max_digits=10,decimal_places=0,null=True)
+
+    fiex_time = models.DateTimeField(null=True)
 
     def __str__(self):
-        return self.title
+        return self.machine_name
--- mytestsite/dashboard/views.py
+++ mytestsite/dashboard/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.utils import timezone
 # Create your views here.
 def index(request):
+    cards = Post.objects.values()
+    # print(cards)
+    for card in cards:
+        
+        card["complete_rate"]="{:.0f}".format( (card['production_capacity'] / card['need_production_capacity'])*100 )
+        card["good_rate"]="{:.0f}".format((card['good_production'] / card['production_capacity']) * 100)
+        card["fiex_time"] = str(timezone.now() - card["fiex_time"]).split(",")[0]
+        # print(timezone.now() - card["fiex_time"])
     context = {
         "block_title":"Dashborad",
-        'cards': Post.objects.all(),
+        'cards': cards,
 
     }
     return render(request, 'dashborad/dashbord.html', context=context)
